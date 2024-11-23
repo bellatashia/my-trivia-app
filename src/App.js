f40b94b88ac7d5d2a740d9ab3be66f9f53082a46
Bella Tashia <bellata@mii.co.id>
2024-11-23 15:09:42 +0700

    Implement the ability to select a trivia category and difficulty level

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const styles = {
+  title: {
+    fontWeight: 'bold',
+  },
+};
+
+const difficulties = ['Easy', 'Medium', 'Hard'];
+
+const getCategoriesUrl = 'https://opentdb.com/api_category.php';
+
 function App() {
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [selectedDifficulty, setSelectedDifficulty] = useState('');
+
+  useEffect(() => {
+    fetch(getCategoriesUrl)
+      .then((response) => response.json())
+      .then((data) => setCategories(data.trivia_categories));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={styles.title}>QUIZ MAKER</h1>
+
+      <select id="categorySelect" value={selectedCategory} onChange={(event) => setSelectedCategory(event.target.value)}>
+        <option value="">Select a Category</option>
+        {categories.map((category) => (
+          <option key={category.id} value={category.id}>
+            {category.name}
+          </option>
+        ))}
+      </select>
+
+      <select id="difficultySelect" value={selectedDifficulty} onChange={(event) => setSelectedDifficulty(event.target.value)}>
+        {difficulties.map((difficulty, index) => (
+          <option key={difficulty + index.toString()} value={difficulty}>
+            {difficulty}
+          </option>
+        ))}
+      </select>
+
+      <button id="createBtn" onClick={() => {}}>
+        Create
+      </button>
     </div>
   );
 }
